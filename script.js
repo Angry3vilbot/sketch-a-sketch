52f15898bcc980999aec536e2c9f0c7694e50f52
Angry3vlibot <myh.balaker@gmail.com>
2022-10-19 16:23:04 +0300

    Write drawing logic, make color selection buttons, make color selection script

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+window.mouseDown = false;
+document.onmousedown = function(){
+	window.mouseDown = true;
+}
+document.onmouseup = function(){
+	window.mouseDown = false;
+}
+var buttons = document.getElementsByTagName('button')
+var color = 'red'
 var grid = document.getElementById('mainContainer')
 for (var i = 0; i <= 255; i++) {
 	var div = document.createElement('div')
 	div.setAttribute('style', 'background-color: white; width: 30px; height: 30px; border: 2px solid black')
+	div.addEventListener('mouseover', changeColor)
 	grid.appendChild(div)
+}
+function changeColor() {
+	if (window.mouseDown) {
+		this.setAttribute('style', `background-color: ${color}; width: 30px; height: 30px; border: 2px solid black`)
+	}
+}
+function changeBrush(col){
+	color = col
 }
